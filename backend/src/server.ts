790cd02bceb22106eef4f265d11f6f6cb7aac3d3
minh.ha <minh.ha@opswat.com>
2026-01-09 18:08:31 +0700

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { routes } from "./routes"
 
 const app = express()
 app.use(express.json())
+
+app.use("/api", routes())
+app.get("/health", (req, res) => {
+	res.status(200).send("OK")
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
